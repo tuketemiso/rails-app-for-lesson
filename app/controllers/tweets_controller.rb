16c3839fd32fcab87f555ee313206152b2111acc
kodera123 <b.b.015.k.k@softbank.ne.jp>
2016-07-23 11:26:43 +0900

    fix comment out

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,11 @@
 class TweetsController < ApplicationController
+  #before_actionを用いてログインしていなければログイン画面へ返す
+  # ヒント: authenticate_〇〇!（deviseのヘルパーメソッド）
 
   def index
-    # allを使ってTweetの全レコードの取得
-    # ページングするためのメソッドを使う
+    # allを使ってTweetの全レコードの取得しインスタンス変数に格納
+      #→ ページングする場合は...  →  参照：「ページング機能を実装しよう」
+        #→  並び順かえたい場合は...  →  「rails order」で検索
     @tweets = Tweet.order(created_at: :desc)
               .page(params[:page]).per(7)
               
@@ -13,12 +16,13 @@ class TweetsController < ApplicationController
   def create
     # ストロングパラメーターを元にインスタンスを生成
     @tweet = Tweet.new(tweet_params)
-    # 投稿したuser_idをtweetのuser_idカラムにセット
+
+    # 投稿したuser_idをtweetのuser_idカラムにセット　→　参照：「[railsブログを作ろう②]ユーザーとポストを紐付けよう！」
     @tweet.user_id = current_user.id
 
     # データベースへ保存する  →  参照：week2「データの保存、抽出」
     @tweet.save
-    # ツイート一覧へリダイレクト　→　参照：week2「簡易掲示板を作ろう３」
+    # ツイート一覧へリダイレクト　→　参照：week2「コントローラーの様々なメソッドを学ぶ」
     redirect_to tweets_path
   end
 
@@ -34,6 +38,7 @@ class TweetsController < ApplicationController
 
   # ここからprivateメソッド
   private
+    #ストロングパラメーターを設定
     def tweet_params
       params.require(:tweet).permit(:content)
     end
